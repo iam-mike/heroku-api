ce53c39ba54110b4a6572712b5f17ff5146a074a
Michael Witts <mawitts@icloud.com>
2020-09-15 08:44:32 +0100

    rewrote cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+var whitelist = ['https://mw-pizza42-app.herokuapp.com', 'http://mw-pizza42-api.herokuapp'];
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
-    methods: 'GET, POST',
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
     credentials: true,
-  }
-  );
+    });
   await app.listen(3000);
 }
 bootstrap();
