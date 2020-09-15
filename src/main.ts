c8b8d6200b966c5f839a04784f8679025ac91ce1
Michael Witts <mawitts@icloud.com>
2020-09-15 08:36:42 +0100

    possible cors fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: true,
+    methods: 'GET, POST';
+    credentials: true,
+  }
+  );
   await app.listen(3000);
 }
 bootstrap();
